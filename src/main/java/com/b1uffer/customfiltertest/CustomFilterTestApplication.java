ce1d68ac4238bc4bed8d8d52cc108bbab31ba604
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 04:21:23 +0900

    first commit

---
+++ src/main/java/com/b1uffer/customfiltertest/CustomFilterTestApplication.java
@@ -0,0 +1,13 @@
+package com.b1uffer.customfiltertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFilterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomFilterTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b1uffer/customfiltertest/CustomFilterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1uffer.customfiltertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFilterTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
