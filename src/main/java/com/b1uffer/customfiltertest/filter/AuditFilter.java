2b2343d066ed065011b7f8aaa66006cf0bf39a0a
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 06:53:30 +0900

    OncePerRequestFilter를 상속한 감사 로그용 AuditFilter 클래스 구현

--- src/main/java/com/b1uffer/customfiltertest/filter/AuditFilter.java
+++ src/main/java/com/b1uffer/customfiltertest/filter/AuditFilter.java
@@ -1,5 +1,31 @@
 package com.b1uffer.customfiltertest.filter;
 
-public class AuditFilter {
-//    GenericFilterBean
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.GenericFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class AuditFilter extends GenericFilter {
+    /**
+     * GenericFilterBean
+     * 감사 로그
+     */
+    private static final Logger log = LoggerFactory.getLogger(AuditFilter.class);
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest http = (HttpServletRequest) servletRequest;
+        String user = (http.getUserPrincipal() != null ? http.getUserPrincipal().getName() : "ANONYMOUS");
+        String uri = http.getRequestURI();
+        log.debug("[AUDIT] user={} uri={}", user, uri);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
