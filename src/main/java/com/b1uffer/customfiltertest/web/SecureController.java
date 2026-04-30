70e92ede956de98220334451ff198b776512f991
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 07:04:11 +0900

    SecureController 구현

--- src/main/java/com/b1uffer/customfiltertest/web/SecureController.java
+++ src/main/java/com/b1uffer/customfiltertest/web/SecureController.java
@@ -1,4 +1,18 @@
 package com.b1uffer.customfiltertest.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecureController {
+    @GetMapping("/api/secure/hello")
+    public String secureHello() {
+        return "This is secure endpoint!";
+    }
+
+    @GetMapping("/api/slow")
+    public String slow() throws InterruptedException {
+        Thread.sleep(1500); // 성능 필터 경고 유발 로직
+        return "slow response done";
+    }
 }
