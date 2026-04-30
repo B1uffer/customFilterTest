aff8e72ff334731d4040e962527580450376ead4
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 06:58:48 +0900

    OncePerRequestFilter를 상속한 publicController 구현

--- src/main/java/com/b1uffer/customfiltertest/web/PublicController.java
+++ src/main/java/com/b1uffer/customfiltertest/web/PublicController.java
@@ -1,4 +1,17 @@
 package com.b1uffer.customfiltertest.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PublicController {
+    @GetMapping("/public/hello")
+    public String hello(
+            @RequestParam(defaultValue = "world") String name,
+            @RequestParam(required = false) String password
+    ) {
+        // password 파라미터는 로깅 필터에서 마스킹이 되는지 필터링용
+        return "Hello" + name + "!";
+    }
 }
