d2327f071445d8f4e8d6603fe21d119f6b839ed3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 07:42:33 +0900

    userDetailsService 추가

--- src/main/java/com/b1uffer/customfiltertest/config/SecurityConfig.java
+++ src/main/java/com/b1uffer/customfiltertest/config/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -32,5 +37,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()); // CSRF 비활성화
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password("password").roles("USER").build(),
+                User.withUsername("admin").password("password").roles("ADMIN").build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
