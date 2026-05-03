1834674aec5c6574516c36df85e8428ad7c818e6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-03 13:08:17 +0900

    http.csrf() TransferController API 구현

--- src/main/java/com/b1uffer/customfiltertest/controller/TransferController.java
+++ src/main/java/com/b1uffer/customfiltertest/controller/TransferController.java
@@ -1,4 +1,25 @@
 package com.b1uffer.customfiltertest.controller;
 
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TransferController {
+
+    @GetMapping("/transfer")
+    public String transferForm(Model model, CsrfToken csrfToken) {
+        model.addAttribute("_csrf", csrfToken);
+        return "transfer";
+    }
+
+    @PostMapping("/transfer")
+    @ResponseBody
+    public String doTransfer(@RequestParam int amount) {
+        return "송금 완료" + amount + "원";
+    }
 }
