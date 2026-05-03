64632a082338a99313777838904558c30d7d0b17
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-03 15:29:18 +0900

    XSS(Cross-Site Scripting) ApiController API 구현

--- src/main/java/com/b1uffer/customfiltertest/controller/ApiController.java
+++ src/main/java/com/b1uffer/customfiltertest/controller/ApiController.java
@@ -1,4 +1,16 @@
 package com.b1uffer.customfiltertest.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class ApiController {
+
+    @GetMapping(value = "/api/data", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> data() {
+        return Map.of("msg", "hello");
+    }
 }
