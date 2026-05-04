de1ea1c763e4ae00a1d5c3d985b9595fb5edec6f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-04 23:52:04 +0900

    XSS(Cross-Site Scripting) 서버에서 nonce 생성, 헤더 주입 CspNonceFilter 구현

--- src/main/java/com/b1uffer/customfiltertest/filter/CspNonceFilter.java
+++ src/main/java/com/b1uffer/customfiltertest/filter/CspNonceFilter.java
@@ -1,4 +1,32 @@
 package com.b1uffer.customfiltertest.filter;
 
-public class CspNonceFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Component
+public class CspNonceFilter extends OncePerRequestFilter {
+    private final SecureRandom secureRandom = new SecureRandom();
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        byte[] b = new byte[16];
+        secureRandom.nextBytes(b);
+
+        String nonce = Base64.getUrlEncoder().withoutPadding().encodeToString(b);
+        request.setAttribute("cspNonce", nonce);
+        response.setHeader("Conent-Security-Policy",
+                "default-src 'self'; script-src 'self' 'nonce-" + nonce + "' " +
+                        "'strict-dynamic'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'");
+
+        filterChain.doFilter(request, response);
+    }
 }
